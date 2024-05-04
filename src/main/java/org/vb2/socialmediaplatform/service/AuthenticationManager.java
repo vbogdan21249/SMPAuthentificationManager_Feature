a280339712949d1692d858324b4f576a268ca4c6
vbogdan21249 <6794117@stud.nau.edu.ua>
2024-05-04 18:01:54 +0300

    AuthenticationManager updated Tested-by: vbogdan21249 <6794117@stud.nau.edu.ua>

--- src/main/java/org/vb2/socialmediaplatform/service/AuthenticationManager.java
+++ src/main/java/org/vb2/socialmediaplatform/service/AuthenticationManager.java
@@ -6,5 +6,31 @@ import java.util.Map;
 import java.util.Set;
 
 public class AuthenticationManager {
+    private final Map<String, String> users; // Map of username and password
+    private final Set<String> loggedInUsers; // Set of currently logged-in users
 
+    public AuthenticationManager() {
+        this.users = new HashMap<>();
+        this.loggedInUsers = new HashSet<>();
+    }
+
+    public void registerUser(String username, String password) {
+        users.put(username, password);
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+            loggedInUsers.add(username);
+            return true;
+        }
+        return false;
+    }
+
+    public void logoutUser(String username) {
+        loggedInUsers.remove(username);
+    }
+
+    public boolean isUserLoggedIn(String username) {
+        return loggedInUsers.contains(username);
+    }
 }
--- src/test/java/org/vb2/socialmediaplatform/service/AuthenticationManagerTest.java
+++ src/test/java/org/vb2/socialmediaplatform/service/AuthenticationManagerTest.java
@@ -6,5 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AuthenticationManagerTest {
-   
+    @Test
+    public void testRegisterUser() {
+        AuthenticationManager authManager = new AuthenticationManager();
+        authManager.registerUser("testUser", "password");
+        assertTrue(authManager.authenticateUser("testUser", "password"));
+    }
+
+    @Test
+    public void testAuthenticateUser() {
+        AuthenticationManager authManager = new AuthenticationManager();
+        authManager.registerUser("testUser", "password");
+        assertTrue(authManager.authenticateUser("testUser", "password"));
+    }
+
+    @Test
+    public void testLogoutUser() {
+        AuthenticationManager authManager = new AuthenticationManager();
+        authManager.registerUser("testUser", "password");
+        authManager.authenticateUser("testUser", "password");
+        authManager.logoutUser("testUser");
+        assertFalse(authManager.isUserLoggedIn("testUser"));
+    }
 }
